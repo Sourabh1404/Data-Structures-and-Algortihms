3ac408b644308e7b2afece8e3aae226c36069c32
Sourabh1404 <80646391+Sourabh1404@users.noreply.github.com>
2022-10-25 22:06:42 +0530

    Update Array_26.java

--- LeetCode/Array/Array_26.java
+++ LeetCode/Array/Array_26.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-for (int i = digits.length - 1; i >= 0; i--) {
-	if (digits[i] < 9) {
-		digits[i]++;
+for (int j = digits.length - 1; j >= 0; j--) {
+	if (digits[j] < 9) {
+		digits[j]++;
 		return digits;
 	}
-	digits[i] = 0;
+	digits[j] = 0;
 }
 
 digits = new int[digits.length + 1];
