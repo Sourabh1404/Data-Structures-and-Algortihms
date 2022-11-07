5017b761a321fc612570c118493e0c48d0de515a
Sourabh1404 <80646391+Sourabh1404@users.noreply.github.com>
2022-11-08 00:07:29 +0530

    Create Array_11.java

---
+++ LeetCode/Array/Array_11.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+
+        int j = -1;
+        if(ruleKey.equals("type"))
+            j=0;
+        else if(ruleKey.equals("color"))
+            j=1;
+        else
+            j=2;
+
+        int count = 0;
+        for(int i=0;i<items.size();i++) {
+            if(items.get(i).get(j).equals(ruleValue))
+                count++;
+        }
+        
+        return count;
+        
+    }
+}
