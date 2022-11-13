b415c2238993c92863cff8676ffb472aa17e65b7
Sourabh Kumar Gupta <sourabh70022@gmail.com>
2022-11-13 22:11:40 +0530

    Update Array_14.java

--- LeetCode/Array/Array_14.java
+++ LeetCode/Array/Array_14.java
@@ -2,18 +2,16 @@ class Solution {
     public int oddCells(int m, int n, int[][] indices) {
      int[][] ans=new int[m][n];
      for(int i=0;i<indices.length;i++){
-         for(int j=0;j<2;j++){
-             if(j==0){
-                for(int k=0;k<n;k++){
-                    ans[indices[i][j]][k]++;
+            int j=0;
+            for(int k=0;k<n;k++){
+                ans[indices[i][j]][k]++;
                 }
-             }
-                else if(j==1)
-                {for(int k=0;k<m;k++){
-                    ans[k][indices[i][j]]++;
-                }}
+             j++;
+            for(int k=0;k<m;k++){
+                ans[k][indices[i][j]]++;
+                }
      }   
-     }
+     
             int count=0;
             for(int[] i:ans){
                 for(int j:i){
