f73e186fda7c46db02c1f320d2a067589001cc57
Sourabh Kumar Gupta <sourabh70022@gmail.com>
2022-11-17 21:31:14 +0530

    Update Search_5.java

--- LeetCode/Searching/Search_5.java
+++ LeetCode/Searching/Search_5.java
@@ -1,7 +1,9 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
     long  start=1;
-     long end=num;
+     long end=num/2;
+     if(num==1)
+     return true;
      while(start<=end){
          long mid=start+(end-start)/2;
          if(mid*mid==num)
