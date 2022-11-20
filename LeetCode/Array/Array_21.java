9d2c8c6ed372e292b1ceebb122d2cab47f9b6239
Sourabh Kumar Gupta <sourabh70022@gmail.com>
2022-11-20 23:39:50 +0530

    Update Array_21.java

--- LeetCode/Array/Array_21.java
+++ LeetCode/Array/Array_21.java
@@ -1,21 +1,14 @@
 class Solution {
-    public int[] twoSum(int[] numbers, int target) {
-        int[] ans=new int[2];
-        int start=0;
-     int end=numbers.length-1;
-     while(true) 
-     {
-         if(numbers[start]+numbers[end]==target)
-         {ans[0]=start;
-         ans[1]=end;
-         return ans;}
-
-         else if(numbers[start]+numbers[end]<target)
-         start++;
-         
-         else
-         end--;
-          }
-        
+    public int[] twoSum(int[] nums, int target) {
+        int[] a=new int[2];
+        for(int i=0;i<nums.length;i++)
+        {
+            for (int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j] == target){
+                    a[0]=i;
+                    a[1]=j;
+                break;}
+            }
+        } return a;
     }
 }
