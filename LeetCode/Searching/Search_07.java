384aded307a6a33317411b60d31a600f317bf5ee
Sourabh Kumar Gupta <sourabh70022@gmail.com>
2022-11-24 01:30:03 +0530

    Update Search_07.java

--- LeetCode/Searching/Search_07.java
+++ LeetCode/Searching/Search_07.java
@@ -2,19 +2,13 @@ class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
        int start=0;
        int end=letters.length-1;
-       char ans=letters[0];
        while(start<=end){
            int mid=(start+end)/2;
            if(letters[mid]>target)
-           {
-            ans=letters[mid];
             end=mid-1;
-           }
-           else if(letters[mid]<target)
+           else 
            start=mid+1;
-           else if (letters[mid]==target)
-           start=mid+1;
-       } 
-       return ans;
+          } 
+       return letters[start%letters.length];
     }
 }
