9b3e4250c4f391abb835a22f574a8221493b1851
Sourabh Kumar Gupta <sourabh70022@gmail.com>
2022-11-23 20:30:19 +0530

    Create Search_09.java

---
+++ LeetCode/Searching/Search_09.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+       int start=0;
+       int end=nums.length-1;
+       int ans=0;
+       while(start<=end){
+           int mid=(start+end)/2;
+           if(nums[mid]==target)
+           return mid;
+           else if(nums[mid]>target)
+           {ans=mid;
+           end=mid-1;
+            }
+            else
+            {  start=mid+1;
+                ans=start; 
+            }
+       } 
+       return ans;
+    }
+}
