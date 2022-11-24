453102adc9c814e880f6be06d2e7c80b541d2a95
Sourabh Kumar Gupta <sourabh70022@gmail.com>
2022-11-24 19:49:29 +0530

    Update Search_1.java

--- LeetCode/Searching/Search_1.java
+++ LeetCode/Searching/Search_1.java
@@ -1,1 +1,19 @@
 
+class Solution {
+    public int mySqrt(int x) {
+    long start=0;
+    long end=x;  
+    long ans=0;
+    while(start<=end){
+    long mid=(start+end)/2;
+        if(mid*mid>x)
+            end=mid-1;
+        else if (mid*mid<x)
+            {start=mid+1;
+            ans=mid;}
+        else 
+        return (int)mid;    
+    }
+    return (int)ans;
+    } 
+}
