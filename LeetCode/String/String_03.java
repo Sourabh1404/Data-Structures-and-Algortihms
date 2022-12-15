94a67e7667648e57f2e43550ce3ccfd10e2466fe
Sourabh Kumar Gupta <sourabh70022@gmail.com>
2022-12-15 20:23:19 +0530

    Create String_03.java

---
+++ LeetCode/String/String_03.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String interpret(String command) {
+    StringBuilder sb=new StringBuilder(); 
+     for(int i=0;i<command.length();i++){
+         if(command.charAt(i)=='G')
+         sb.append("G");
+         else if(command.charAt(i)=='(' && command.charAt(i+1)=='a')
+         {
+             sb.append("al");
+             i+=3;
+         }
+         else
+         {
+             sb.append("o");
+             i+=1;
+         }
+     } 
+     return new String(sb);  
+    }
+}
