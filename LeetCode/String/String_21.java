27195d868b2a54e0756530cd3d9efd087ee4e3b4
Sourabh Kumar Gupta <sourabh70022@gmail.com>
2022-12-30 01:46:29 +0530

    Update String_21.java

--- LeetCode/String/String_21.java
+++ LeetCode/String/String_21.java
@@ -1,3 +1,4 @@
+//Solution1//
 class Solution {
     public String mergeAlternately(String word1, String word2) {
        StringBuilder sb=new StringBuilder();
@@ -12,6 +13,23 @@ class Solution {
         while(j<word2.length()){
             sb.append(word2.charAt(j++));
         }
+        return sb.toString();
+    }
+}
+
+
+//Solution2//
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+       StringBuilder sb=new StringBuilder();
+        int len=word1.length()+word2.length();
+        for(int i=0;i<len;i++){
+            if(i<word1.length())
+            sb.append(word1.charAt(i));
+            if(i<word2.length())
+            sb.append(word2.charAt(i));
+        }
+       
         return sb.toString();
     }
 }
