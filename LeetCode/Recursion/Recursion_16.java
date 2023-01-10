6d40f4dcc0d7a0d1803076662431cbebb4b41f0b
Sourabh Kumar Gupta <sourabh70022@gmail.com>
2023-01-10 23:23:02 +0530

    Update Recursion_16.java

--- LeetCode/Recursion/Recursion_16.java
+++ LeetCode/Recursion/Recursion_16.java
@@ -1,3 +1,5 @@
+//Solution1//
+
 class Solution {
     public boolean isPowerOfTwo(int n) {
   long m=1;
@@ -5,5 +7,18 @@ class Solution {
   m=m*2;
 
   return m==n;
+    }
+}
+
+//Solution2//
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+   if (n<1)
+      return false;
+    if(n%2==0)
+      return isPowerOfTwo(n/2);
+
+      return n==1; 
     }
 }
