1e7786ed94537b3625603c8056fa36a53cba1a50
Sourabh Kumar Gupta <sourabh70022@gmail.com>
2023-01-27 23:16:07 +0530

    Create Bitwise_03.java

---
+++ LeetCode/Bitwise/Bitwise_03.java
@@ -0,0 +1,15 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+       int res = 0;
+        for (int i = 0; i < 32; i++) {
+            // shift to the left, i.e. ...001 is changed into ...010
+            res <<= 1; 
+            // n&1 - keep 1 at the end of n if it ends with 1
+            // | if n ends with 1, then res is also ends with 1
+            res = res | (n & 1); 
+            n >>= 1; // shift n to the right for 1 position
+        }
+        return res; 
+    }
+}
