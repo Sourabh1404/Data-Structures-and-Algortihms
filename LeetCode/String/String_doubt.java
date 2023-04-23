bdd691e0136d6019ad4f3acd4f1a6fa89956b0b6
Sourabh Kumar Gupta <sourabh70022@gmail.com>
2023-04-23 23:36:33 +0530

    Update String_doubt.java

--- LeetCode/String/String_doubt.java
+++ LeetCode/String/String_doubt.java
@@ -21,3 +21,27 @@ class Solution {
         return (int) totalCount;
     }
 }
+
+class Solution {
+    public int countHomogenous(String s) {
+        final int MODULO = 1000000007;
+        long totalCount = 0;
+        int length = s.length();
+        char prev = '0';
+        int consecutive = 0;
+        for (int i = 0; i < length; i++) {
+            char c = s.charAt(i);
+            if (c == prev)
+                consecutive++;
+            else {
+                long curCount = (long) consecutive * (consecutive + 1) / 2 % MODULO;
+                totalCount = (totalCount + curCount) % MODULO;
+                consecutive = 1;
+                prev = c;
+            }
+        }
+        long curCount = (long) consecutive * (consecutive + 1) / 2 % MODULO;
+        totalCount = (totalCount + curCount) % MODULO;
+        return (int) totalCount;
+    }
+}
